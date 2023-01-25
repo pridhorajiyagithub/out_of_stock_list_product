8dd8459db2848cb94a12a5e7c7235e07a8b908b6
pridhorajiyagithub <111557931+pridhorajiyagithub@users.noreply.github.com>
2023-01-25 09:49:23 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
-import 'dart:async';
-import 'dart:typed_data';
 import 'dart:core';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:out_of_stock_product/provider/add_to_cart.dart';
-import 'package:out_of_stock_product/views/screens.dart';
+
+import 'package:product_db/provider/add_to_cart.dart';
+import 'package:product_db/views/screens.dart';
 import 'package:provider/provider.dart';
-import 'db helper.dart';
-import 'model/product.dart';
-import 'package:flutter_image_compress/flutter_image_compress.dart';
 
 void main() {
   runApp(MultiProvider(
